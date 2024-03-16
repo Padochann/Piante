97ec3d855340396bc7d66320d83409d040240071
Marco Rossetti <rossettimarco885@gmail.com>
2024-03-16 12:54:44 +0100

    update window java

--- Piante/src/view/Window.java
+++ Piante/src/view/Window.java
@@ -6,11 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.SpringLayout;
+import java.awt.CardLayout;
 
 public class Window extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JPanel panel;
+	private JPanel panel_1;
+	private JPanel panel_2;
 
 	/**
 	 * Launch the application.
@@ -38,6 +43,21 @@ public class Window extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		SpringLayout sl_contentPane = new SpringLayout();
+		contentPane.setLayout(sl_contentPane);
+		
+		panel = new JPanel();
+		sl_contentPane.putConstraint(SpringLayout.NORTH, panel, 10, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, panel, 120, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, panel, 104, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, panel, 285, SpringLayout.WEST, contentPane);
+		contentPane.add(panel);
+		panel.setLayout(new CardLayout(0, 0));
+		
+		panel_1 = new JPanel();
+		panel.add(panel_1, "name_945515184868000");
+		
+		panel_2 = new JPanel();
+		panel.add(panel_2, "name_945526498430100");
 	}
-
 }
